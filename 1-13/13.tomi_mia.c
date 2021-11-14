4bb9d8f60e6ee6508719f9c7d7082de45a6374bf
MD TAZRI <mdtazri@gmail.com>
2021-11-14 23:03:16 +0600

    solve some problem in problem number 13

--- 1-13/13.tomi_mia.c
+++ 1-13/13.tomi_mia.c
@@ -4,12 +4,16 @@
 // function prototype
 int word_count(char []); // find how many word in string;
 int fact(int); // find facterial
+int simillar_word_fact(char [],int); // find similar word facterial in word 
 
 int main(void){
     // create necessary variable
     int test, // test for test case
         length,
-        total_word; // store sentence length
+        total_word, // store sentence length
+        fact_simillar_word, // facterial of simillar word
+        fact_total_word; // store total_word! (facterial)
+
     char sentence[220]; // for sentence
 
     // get test
@@ -22,8 +26,11 @@ int main(void){
         // get sentence from user
         gets(sentence);
 
-        // calculate total word
+        // calculate total_word!
         total_word = word_count(sentence);
+        fact_total_word = fact(total_word);
+        fact_simillar_word =  simillar_word_fact(sentence,total_word);
+        printf("fact simillar word %d\n",fact_simillar_word);
     }
     
 
@@ -60,10 +67,54 @@ int fact(int n){
     int i,facterial = 1;
     
     // find facterial
-    for(i = n; n > 1;i--){
+    for(i = 2; i < n;i++){
         facterial*=i;
     }
 
     // return facterial
+    return facterial;
+}
+
+// similar_word_fact(char [],int)
+int simillar_word_fact(char s[],int total_word){
+    // create necessary variable
+    char words[total_word][25];
+    char copy_words[total_word][25];
+    int i, // iterate the sentence(s)
+        word_index = 0, // word index in words;
+        j = 0, // iterate the word
+        k, // for iterate
+        start = 0, // is start the word
+        facterial = 1, // for simillar word facterial
+        total_copy_word = 0, // number of element of copy_words
+        simillar = 1; // find how many simillar word
+
+    // start traverse sentence and collect the word into the words
+    for(i = 0; i < strlen(s);i++){
+        // if s[i] != ' '; starting the word
+        if(s[i] != ' ' && !start){
+            start = 1;
+        }
+
+        // if start = 1 then collect the word
+        if(start){
+            words[word_index][j] = s[i];
+            j++;
+        }
+
+        // if finish the word
+        // if s[i] == ' ' && s[i-1] != ' '
+        if(s[i] == ' ' && s[i-1] != ' '){
+            start = 0;
+            words[word_index][j] = '\0';
+            word_index++;
+            j = 0;
+        }
+    }
+    words[word_index][j] = '\0';
+
+    
+
+    // return the facterial
     return facterial;
 }
